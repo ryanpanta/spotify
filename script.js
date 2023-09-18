f36c6afb64339dc34b1bb18aad329987cf845d33
Ryan Rodrigues <ryan.rodrigues62@hotmail.com>
2023-09-18 15:12:36 -0300

    mais atualizacao - base finalizada

--- script.js
+++ script.js
@@ -5,24 +5,41 @@ const song = document.getElementById("audio")
 const play = document.getElementById("play")
 const previous = document.getElementById("previous")
 const next = document.getElementById("next")
+const currentProgress = document.querySelector(".current-progress")
+const progressContainer = document.querySelector(".progress-container")
+const shuffle = document.getElementById("shuffle")
+const repeat = document.getElementById("repeat")
+const songTime = document.getElementById("song-time")
+const totalTime = document.getElementById("total-time")
+const like = document.getElementById("like")
 
-songName.innerHTML = "Como um Anjo"
-bandName.innerHTML = "Os Mennoti"
 let isPlaying = false
+let isShuffle = false
+let repeatOn = false
 
 const comoUmAnjo = {
     songName: "Como um Anjo",
     artist: "César Menotti & Fabiano",
-    file: "como-um-anjo"
+    file: "como-um-anjo",
+    liked: false
 }
 
 const anjos = {
     songName: "Anjos (Pra quem tem fé)",
     artist: "O Rappa",
-    file: "anjos"
+    file: "anjos",
+    liked: false
 }
 
-const playlist = [comoUmAnjo, anjos]
+const existeUmLugar = {
+    songName : "Mas Existe um Lugar - Remix",
+    artist: "Cryzin, Manoel Gomes, Kaio Viana, Noemi Leal",
+    file: "existe-um-lugar",
+    liked: false
+}
+
+const originalPlaylist = JSON.parse(localStorage.getItem('playlist')) ?? [comoUmAnjo, anjos, existeUmLugar]
+let sortedPlaylist = [...originalPlaylist]
 let index = 0
 
 function playSong() {
@@ -44,16 +61,28 @@ function playPauseDecider(){
         playSong()
 }
 
+function likeButtonRender(){
+    if(sortedPlaylist[index].liked === true) {
+        like.querySelector(".bi").classList = "bi bi-heart-fill"
+        like.classList.add("button-active")
+    } else {
+        like.querySelector(".bi").classList = "bi bi-heart"
+        like.classList.remove("button-active")
+    }
+}   
+
+
 function initializeSong(){
-    cover.src = `images/${playlist[index].file}.jpg`
-    song.src = `songs/${playlist[index].file}.mp3`
-    songName.innerHTML = playlist[index].songName
-    bandName.innerHTML = playlist[index].artist
+    cover.src = `images/${sortedPlaylist[index].file}.jpg`
+    song.src = `songs/${sortedPlaylist[index].file}.mp3`
+    songName.innerHTML = sortedPlaylist[index].songName
+    bandName.innerHTML = sortedPlaylist[index].artist
+    likeButtonRender()
 }
 
 function previousSong(){
     if(index === 0)
-        index = playlist.length-1
+        index = sortedPlaylist.length-1
     else
         index -= 1
     initializeSong()
@@ -61,7 +90,7 @@ function previousSong(){
 }
 
 function nextSong(){
-    if(index === playlist.length-1)
+    if(index === sortedPlaylist.length-1)
         index = 0
     else
         index += 1
@@ -69,10 +98,101 @@ function nextSong(){
     playSong()
 }
 
+function updateProgress(){
+    const barWidth = (song.currentTime / song.duration) * 100
+    currentProgress.style.setProperty("--progress", `${barWidth}%`)
+    //currentProgress.style.width = `${barWidth}%`
+    updateCurrentTime()
+}
+
+function jumpTo(event){
+    const width = progressContainer.clientWidth
+    const clickPosition = event.offsetX
+    const jumpToTime = (clickPosition / width) * song.duration
+    song.currentTime = jumpToTime
+}   
+
+function shuffleArray(preShuffleArray){
+    let currentIndex = preShuffleArray.length - 1
+    while(currentIndex > 0){
+        let randomIndex = Math.floor(Math.random() * sortedPlaylist.length)
+        let aux = preShuffleArray[currentIndex]
+        preShuffleArray[currentIndex] = preShuffleArray[randomIndex]
+        preShuffleArray[randomIndex] = aux
+        currentIndex--;
+    }
+}
+
+function shuffleButtonClicked(){
+    if(isShuffle === false) {
+        isShuffle = true
+        shuffleArray(sortedPlaylist)
+        shuffle.classList.add("button-active")
+    }
+    else{
+        isShuffle = false
+        sortedPlaylist = [...originalPlaylist]
+        shuffle.classList.remove("button-active")
+    }
+}
+
+function repeatButtonClicked(){
+    if(repeatOn === false){
+        repeatOn = true
+        repeat.classList.add("button-active")
+    }
+    else {
+        repeatOn = false
+        repeat.classList.remove("button-active")
+    }    
+}
+
+function nextOrRepeat(){
+    if(repeatOn === false)
+        nextSong()
+    else
+        playSong()
+}
+
+function toMMSS(originalNumber) {
+    let hours = Math.floor(originalNumber / 3600)
+    let min = Math.floor((originalNumber - hours * 3600) / 60)
+    let secs = Math.floor(originalNumber - hours * 3600 - min * 60)
+    
+    return `${min.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`   
+}
+
+function updateCurrentTime(){
+    songTime.innerHTML = toMMSS(song.currentTime)
+}
+
+function updateTotalTime(){
+    totalTime.innerHTML = toMMSS(song.duration)
+}
+
+function likeButtonClicked(){
+    if(sortedPlaylist[index].liked === false) {
+        sortedPlaylist[index].liked = true
+    } else {
+        sortedPlaylist[index].liked = false
+    }
+    likeButtonRender()
+    localStorage.setItem('playlist', JSON.stringify(originalPlaylist));
+
+}
+
 initializeSong()
+
 
 play.addEventListener("click", playPauseDecider)
 previous.addEventListener("click", previousSong)
 next.addEventListener("click", nextSong)
+song.addEventListener("timeupdate", updateProgress)
+song.addEventListener("ended", nextOrRepeat)
+song.addEventListener("loadedmetadata", updateTotalTime) 
+progressContainer.addEventListener("click", jumpTo)
+shuffle.addEventListener("click", shuffleButtonClicked)
+repeat.addEventListener("click", repeatButtonClicked)
+like.addEventListener("click", likeButtonClicked)
 
 
